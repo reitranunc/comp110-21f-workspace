6b79debc6ac7dba83808d66dedd7c7d4042a77d5
Reilly Tran <reitran@unc.edu>
2021-08-30 13:31:30 -0400

    Fixed White Space Feedback Error

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,8 +1,8 @@
-"""hype machine generator with user input"""
+"""hype machine generator with user input."""
 
-__author__=str("730397081")
+__author__ = str("730397081")
 
-name: str=input("What is your name? ")
+name: str = input("What is your name? ")
 print(name + ", you are amazing!")
 print("It's a great day to have a great day " + name + "!")
-print ("What are you still doing here " + name + "? It's time to go and seize the day!")
+print("What are you still doing here " + name + "? It's time to go and seize the day!")
--- exercises/ex01/numeric_operators.py
+++ exercises/ex01/numeric_operators.py
@@ -1,9 +1,9 @@
-"""numeric operators program with user input"""
-__author__=str("730397081")
+"""numeric operators program with user input."""
+__author__ = str("730397081")
 
-left_hand_side=int(input("Left-hand side: "))
-right_hand_side=int(input("Right-hand side: "))
-print(str(left_hand_side)+str(" ** ")+ str(right_hand_side)+" is "+str(left_hand_side ** right_hand_side))
-print(str(left_hand_side)+str(" / ")+ str(right_hand_side)+" is "+str(left_hand_side / right_hand_side))
-print(str(left_hand_side)+str(" // ")+ str(right_hand_side)+" is "+str(left_hand_side // right_hand_side))
-print(str(left_hand_side)+str(" % ")+ str(right_hand_side)+" is "+str(left_hand_side % right_hand_side))
+left_hand_side = int(input("Left-hand side: "))
+right_hand_side = int(input("Right-hand side: "))
+print(str(left_hand_side) + str(" ** ") + str(right_hand_side) + " is " + str(left_hand_side ** right_hand_side))
+print(str(left_hand_side) + str(" / ") + str(right_hand_side) + " is " + str(left_hand_side / right_hand_side))
+print(str(left_hand_side) + str(" // ") + str(right_hand_side) + " is " + str(left_hand_side // right_hand_side))
+print(str(left_hand_side) + str(" % ") + str(right_hand_side) + " is " + str(left_hand_side % right_hand_side))
